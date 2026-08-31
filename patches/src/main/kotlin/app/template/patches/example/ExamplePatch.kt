c65d46960c8c8d2d1a692ce510c00a15ef285650
HelioFloxZ <tizenbacon@gmail.com>
2026-08-31 17:30:59 +0530

    chore: ExamplePatch.kt

--- patches/src/main/kotlin/app/template/patches/example/ExamplePatch.kt
+++ patches/src/main/kotlin/app/template/patches/example/ExamplePatch.kt
@@ -4,35 +4,25 @@ import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.patch.bytecodePatch
 import app.template.patches.shared.Constants.COMPATIBILITY_EXAMPLE
 
-private const val EXTENSION_CLASS = "Lapp/template/extension/extension/ExamplePatch;"
+private const val EXTENSION_CLASS = "Lapp/template/extension/ExamplePatch;"
 
 @Suppress("unused")
 val examplePatch = bytecodePatch(
-    name = "YouTube Studio Enhancements",
-    description = "Disables update checks, redirects, telemetry, and enables MicroG support.",
+    name = "Example Patch",
+    description = "Example patch to start with.",
     default = true
 ) {
     dependsOn(internalPatch)
 
     extendWith("extensions/extension.mpe")
 
-    compatibleWith(COMPATIBILITY_EXAMPLE)
-
     execute {
-        exampleFingerprint.method.addInstructions(
+        AdLoaderFingerprint.method.addInstructions(
             0,
             """
-                const/4 v0, 0x0
+                invoke-static {}, $EXTENSION_CLASS;->showAds()Z
+                move-result v0
                 return v0
-            """
-        )
-
-        exampleIntegrationsFingerprint.method.addInstructions(
-            0,
-            """
-                invoke-static {}, $EXTENSION_CLASS;->getGmsCorePackageName()Ljava/lang/String;
-                move-result-object v0
-                return-object v0
             """
         )
     }
