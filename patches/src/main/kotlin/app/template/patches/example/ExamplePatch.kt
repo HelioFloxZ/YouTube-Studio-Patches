d79535da0a8fcee01997fc06b55ae0cc3009dd11
HelioFloxZ <tizenbacon@gmail.com>
2026-08-31 17:25:33 +0530

    chore: Update ExamplePatch.kt

--- patches/src/main/kotlin/app/template/patches/example/ExamplePatch.kt
+++ patches/src/main/kotlin/app/template/patches/example/ExamplePatch.kt
@@ -7,15 +7,19 @@ import app.template.patches.shared.Constants.COMPATIBILITY_EXAMPLE
 private const val EXTENSION_CLASS = "Lapp/template/extension/extension/ExamplePatch;"
 
 @Suppress("unused")
-val disableUpdatesPatch = bytecodePatch(
-    name = "Disable Updates",
-    description = "Disables in-app update prompts and Play Store update redirects.",
+val examplePatch = bytecodePatch(
+    name = "YouTube Studio Enhancements",
+    description = "Disables update checks, redirects, telemetry, and enables MicroG support.",
     default = true
 ) {
     dependsOn(internalPatch)
 
+    extendWith("extensions/extension.mpe")
+
+    compatibleWith(COMPATIBILITY_EXAMPLE)
+
     execute {
-        updateCheckFingerprint.method.addInstructions(
+        exampleFingerprint.method.addInstructions(
             0,
             """
                 const/4 v0, 0x0
@@ -23,45 +27,7 @@ val disableUpdatesPatch = bytecodePatch(
             """
         )
 
-        playStoreRedirectFingerprint.method.addInstructions(
-            0,
-            """
-                return-void
-            """
-        )
-    }
-}
-
-@Suppress("unused")
-val disableTelemetryPatch = bytecodePatch(
-    name = "Disable Telemetry",
-    description = "Disables analytics and telemetry event logging.",
-    default = true
-) {
-    dependsOn(internalPatch)
-
-    execute {
-        telemetryLoggerFingerprint.method.addInstructions(
-            0,
-            """
-                return-void
-            """
-        )
-    }
-}
-
-@Suppress("unused")
-val microGSupportPatch = bytecodePatch(
-    name = "MicroG Support",
-    description = "Redirects Google Play Services calls to GmsCore (MicroG).",
-    default = true
-) {
-    dependsOn(internalPatch)
-
-    extendWith("extensions/extension.mpe")
-
-    execute {
-        gmsCoreRedirectionFingerprint.method.addInstructions(
+        exampleIntegrationsFingerprint.method.addInstructions(
             0,
             """
                 invoke-static {}, $EXTENSION_CLASS;->getGmsCorePackageName()Ljava/lang/String;
