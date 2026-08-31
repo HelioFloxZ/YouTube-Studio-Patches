aa3c83b979aa2686942191665c31834d5b906f2a
HelioFloxZ <tizenbacon@gmail.com>
2026-08-31 17:24:49 +0530

    chore: Update Constants.kt

--- patches/src/main/kotlin/app/template/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/template/patches/shared/Constants.kt
@@ -1,21 +1,10 @@
 package app.template.patches.shared
 
-import app.morphe.patcher.patch.ApkFileType
-import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
 
 object Constants {
-    const val YT_STUDIO_PACKAGE_NAME = "com.google.android.apps.youtube.creator"
-
     val COMPATIBILITY_EXAMPLE = Compatibility(
         name = "YouTube Studio",
-        packageName = YT_STUDIO_PACKAGE_NAME,
-        apkFileType = ApkFileType.APK,
-        appIconColor = 0xFF0000,
-        targets = listOf(
-            AppTarget(
-                version = null
-            )
-        )
+        packageName = "com.google.android.apps.youtube.creator"
     )
 }
