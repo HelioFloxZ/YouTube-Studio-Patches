c0f46f7b899102901c586cd9d1224627fb2d70e7
HelioFloxZ <tizenbacon@gmail.com>
2026-08-31 17:20:08 +0530

    chore: Update ExamplePatch.kt

--- patches/src/main/kotlin/app/template/patches/example/ExamplePatch.kt
+++ patches/src/main/kotlin/app/template/patches/example/ExamplePatch.kt
@@ -1,23 +1,73 @@
 package app.template.patches.example
 
-import app.morphe.patcher.fingerprint.methodFingerprint
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
+import app.morphe.patcher.patch.bytecodePatch
+import app.template.patches.shared.Constants.COMPATIBILITY_EXAMPLE
 
-val updateCheckFingerprint = methodFingerprint(
-    returnType = "Z",
-    strings = listOf("force_update", "is_update_required")
-)
+private const val EXTENSION_CLASS = "Lapp/template/extension/extension/ExamplePatch;"
 
-val playStoreRedirectFingerprint = methodFingerprint(
-    returnType = "V",
-    strings = listOf("market://details?id=com.google.android.apps.youtube.creator")
-)
+@Suppress("unused")
+val disableUpdatesPatch = bytecodePatch(
+    name = "Disable Updates",
+    description = "Disables in-app update prompts and Play Store update redirects.",
+    default = true
+) {
+    dependsOn(internalPatch)
 
-val telemetryLoggerFingerprint = methodFingerprint(
-    returnType = "V",
-    strings = listOf("ClearcutLogger", "logEvent")
-)
+    execute {
+        updateCheckFingerprint.method.addInstructions(
+            0,
+            """
+                const/4 v0, 0x0
+                return v0
+            """
+        )
 
-val gmsCoreRedirectionFingerprint = methodFingerprint(
-    returnType = "Ljava/lang/String;",
-    strings = listOf("com.google.android.gms")
-)
+        playStoreRedirectFingerprint.method.addInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+    }
+}
+
+@Suppress("unused")
+val disableTelemetryPatch = bytecodePatch(
+    name = "Disable Telemetry",
+    description = "Disables analytics and telemetry event logging.",
+    default = true
+) {
+    dependsOn(internalPatch)
+
+    execute {
+        telemetryLoggerFingerprint.method.addInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+    }
+}
+
+@Suppress("unused")
+val microGSupportPatch = bytecodePatch(
+    name = "MicroG Support",
+    description = "Redirects Google Play Services calls to GmsCore (MicroG).",
+    default = true
+) {
+    dependsOn(internalPatch)
+
+    extendWith("extensions/extension.mpe")
+
+    execute {
+        gmsCoreRedirectionFingerprint.method.addInstructions(
+            0,
+            """
+                invoke-static {}, $EXTENSION_CLASS;->getGmsCorePackageName()Ljava/lang/String;
+                move-result-object v0
+                return-object v0
+            """
+        )
+    }
+}
