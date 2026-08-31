34b98dcc000e3ccd985c951d3641807976965ff5
HelioFloxZ <tizenbacon@gmail.com>
2026-08-31 17:25:14 +0530

    chore: Update Fingerprints.kt

--- patches/src/main/kotlin/app/template/patches/example/Fingerprints.kt
+++ patches/src/main/kotlin/app/template/patches/example/Fingerprints.kt
@@ -2,22 +2,12 @@ package app.template.patches.example
 
 import app.morphe.patcher.fingerprint.methodFingerprint
 
-val updateCheckFingerprint = methodFingerprint(
+val exampleFingerprint = methodFingerprint(
     returnType = "Z",
-    strings = listOf("force_update", "is_update_required")
+    parameters = listOf()
 )
 
-val playStoreRedirectFingerprint = methodFingerprint(
-    returnType = "V",
-    strings = listOf("market://details?id=com.google.android.apps.youtube.creator")
-)
-
-val telemetryLoggerFingerprint = methodFingerprint(
-    returnType = "V",
-    strings = listOf("ClearcutLogger", "logEvent")
-)
-
-val gmsCoreRedirectionFingerprint = methodFingerprint(
+val exampleIntegrationsFingerprint = methodFingerprint(
     returnType = "Ljava/lang/String;",
-    strings = listOf("com.google.android.gms")
+    parameters = listOf()
 )
