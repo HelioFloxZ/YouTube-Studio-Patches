ba685f6daf165b7b606d3e1dc20e4f679646dd2b
HelioFloxZ <tizenbacon@gmail.com>
2026-09-01 22:58:40 +0530

    fix: Fixed ExamplePatch.kt

--- patches/src/main/kotlin/app/template/patches/example/ExamplePatch.kt
+++ patches/src/main/kotlin/app/template/patches/example/ExamplePatch.kt
@@ -1,29 +1,24 @@
 package app.template.patches.example
 
-import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
+import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
 import app.morphe.patcher.patch.bytecodePatch
 import app.template.patches.shared.Constants.COMPATIBILITY_EXAMPLE
 
-private const val EXTENSION_CLASS = "Lapp/template/extension/ExamplePatch;"
-
 @Suppress("unused")
-val examplePatch = bytecodePatch(
-    name = "Example Patch",
-    description = "Example patch to start with.",
+val youtubeStudioPatch = bytecodePatch(
+    name = "YouTube Studio Update Blocker",
+    description = "Blocks the identified update action and Play Store update redirect.",
     default = true
 ) {
-    dependsOn(internalPatch)
-
-    extendWith("extensions/extension.mpe")
-
     execute {
-        AdLoaderFingerprint.method.addInstructions(
+        StartUpdateFingerprint.method.addInstruction(
             0,
-            """
-                invoke-static {}, $EXTENSION_CLASS;->showAds()Z
-                move-result v0
-                return v0
-            """
+            "return-void"
+        )
+
+        PlayStoreRedirectFingerprint.method.addInstruction(
+            0,
+            "return-void"
         )
     }
 }
