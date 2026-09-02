39ef691ffd85bcb33be4aa826cfda5d14c88dc38
HelioFloxZ <tizenbacon@gmail.com>
2026-09-02 07:48:10 +0530

    chore: update GmsCorePatch.kt

--- patches/src/main/kotlin/app/template/patches/example/GmsCorePatch.kt
+++ patches/src/main/kotlin/app/template/patches/example/GmsCorePatch.kt
@@ -1,4 +1,4 @@
-package app.template.patches.ytstudio
+package app.template.patches.example
 
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
